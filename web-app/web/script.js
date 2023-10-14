1bc37b055521efd2c53e5bebb213b10f78bae788
emiliocramer <emilio.cramer101@gmail.com>
2023-10-14 13:14:24 +0200

    serve and pull apps every 3 secs and display text of relevant apps

--- web-app/web/script.js
+++ web-app/web/script.js
@@ -1,12 +1,14 @@
 async function checkKeyboard() {
-    console.log("...beginning keyboard check")
+    console.log("...beginning keyboard check");
+    const connectButton = document.getElementById('connectButton');
+
     try {
         const devices = await navigator.hid.requestDevice({
             filters: [{vendorId: 0x574c}]
         });
 
         if (devices.length === 0) {
-            console.log("No devices found")
+            console.log("No devices found");
             alert('No device found');
             return;
         }
@@ -14,8 +16,41 @@ async function checkKeyboard() {
         const device = devices[0];
         await device.open();
         console.log('Device opened:', device);
-        // Now you can communicate with the device
+
+        // once connected
+        connectButton.textContent = "Connected Keyboard";
+        connectButton.style.backgroundColor = "green";
+        connectButton.style.color = "white";
+        connectButton.disabled = true;
+
     } catch (error) {
         console.error('Error:', error);
     }
 }
+
+async function fetchRunningApps() {
+    console.log("fetchRunningApps function is being called");
+    try {
+        const response = await fetch('http://localhost:8080/runningApps');
+        const apps = await response.json();
+        console.log("f3tching open apps...")
+        const supportedApps = ["XCODE", "PHOTOSHOP"];
+        const detectedSupportedApps = apps.filter(app => supportedApps.includes(app.toUpperCase()));
+
+        // Update the UI with detected apps
+        const container = document.getElementById('detectedApps');
+        container.innerHTML = '';  // Clear previous apps
+        detectedSupportedApps.forEach(app => {
+            const appElement = document.createElement('div');
+            appElement.textContent = app;
+            container.appendChild(appElement);
+        });
+        console.log("Detected supported apps:", detectedSupportedApps);
+    } catch (error) {
+        console.error('Error fetching running apps:', error);
+    }
+}
+
+fetchRunningApps();
+setInterval(fetchRunningApps, 3000);
+
